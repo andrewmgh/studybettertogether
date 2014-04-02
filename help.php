2be3f2c5ce1e216dbb47233d05c00fcbffed55a2
Andrew Monaghan <andrewmgh@gmail.com>
2014-04-02 21:13:56 +0100

    User manual linked to Help page

--- help.php
+++ help.php
@@ -3,8 +3,37 @@
 
 
 <h2 class = "mainPageHeading">Help Section </h2>
-<p> Coming Soon....</p>	
+
+<div class = "pdf">
+<!-- <iframe
+src="//www.scribd.com/embeds/206371602/content?start_page=3&view_mode=scroll&access_key=key-72cpcebqo2jnx1chgvx&show_recommendations=true" 
+data-auto-height="false" 
+data-aspect-ratio="0.804456606724003" 
+scrolling="no" 
+id="doc_40649" 
+width=850" 
+height="1000" 
+frameborder="0">
+</iframe> -->
 
 
+<iframe
+src="http://www.studybettertogether.com/resources/SBT_UserManual_Draft1.pdf" 
+data-auto-height="false" 
+scrolling="yes" 
+type = "application/pdf"
+width=800" 
+height="800" 
+frameborder="1">
+</iframe> 
+
+
+
+
+
+
+
+
+</div>
 
 <?php require_once 'includes/html_template/footer.php';?>
