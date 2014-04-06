0c0690e0c3dcb56bb3f0835e67afa99d33f6f182
Andrew Monaghan <andrewmgh@gmail.com>
2014-04-06 11:06:09 +0100

    Further work on user manual

--- help.php
+++ help.php
@@ -5,18 +5,6 @@
 <h2 class = "mainPageHeading">Help Section </h2>
 
 <div class = "pdf">
-<!-- <iframe
-src="//www.scribd.com/embeds/206371602/content?start_page=3&view_mode=scroll&access_key=key-72cpcebqo2jnx1chgvx&show_recommendations=true" 
-data-auto-height="false" 
-data-aspect-ratio="0.804456606724003" 
-scrolling="no" 
-id="doc_40649" 
-width=850" 
-height="1000" 
-frameborder="0">
-</iframe> -->
-
-
 <iframe
 src="http://www.studybettertogether.com/resources/SBT_UserManual_Draft1.pdf" 
 data-auto-height="false" 
@@ -26,14 +14,6 @@ width=800"
 height="800" 
 frameborder="1">
 </iframe> 
-
-
-
-
-
-
-
-
 </div>
 
 <?php require_once 'includes/html_template/footer.php';?>
